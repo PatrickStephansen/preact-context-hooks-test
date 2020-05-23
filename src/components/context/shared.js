a70ecc77f660851114c3383464b74dd4b4bf57f4
Patrick Stephansen <patrick@entelect.co.za>
2020-05-23 17:38:26 +0200

    simplify demo to only change message twice

--- src/components/context/shared.js
+++ src/components/context/shared.js
@@ -1,30 +1,16 @@
 import { useEffect, useContext } from "preact/hooks";
 
 export const delay = (value) =>
-  new Promise((resolve) => setTimeout(() => resolve(value), 1000));
+  new Promise((resolve) => setTimeout(() => resolve(value), 5000));
 
 export const reducer = (state, action) => {
   console.groupCollapsed(`action received ${action.type}`);
   console.log("action", action);
-  if (action.type === "effectTest") {
-    Promise.resolve().then(() =>
-      state.dispatch({
-        type: "setMessage",
-        payload: "From effect with wrapper dispatcher immediate",
-      })
-    );
-    delay().then(() =>
-      state.dispatch({
-        type: "setMessage",
-        payload: "From effect with wrapper dispatcher delayed",
-      })
-    );
-  }
   const newState =
     action.type === "setMessage"
       ? { ...state, innerMessage: action.payload }
-      : action.type === "enableEffects"
-      ? { ...state, dispatch: action.payload }
+      : action.type === "activate"
+      ? { ...state, active: true }
       : state;
   console.log("new state", newState);
   console.groupEnd();
@@ -37,12 +23,13 @@ export const reducer = (state, action) => {
 export const ContextMessage = ({ context }) => {
   const [{ innerMessage }, dispatch] = useContext(context);
   useEffect(() => {
-    const showDelayed = (m) =>
-      delay(m).then((m) => dispatch({ type: "setMessage", payload: m }));
-    showDelayed("first message")
-      .then(() => showDelayed("second message"))
-      .then(() => delay())
-      .then(() => dispatch({ type: "effectTest" }));
+    dispatch({
+      type: "setMessage",
+      payload: "should show immediately (synchronous change works)",
+    });
+    delay("should show later (asynchronous change works)").then((message) =>
+      dispatch({ type: "setMessage", payload: message })
+    );
   }, []);
   console.log("rendering ContextMessage with message", innerMessage);
   return (
@@ -56,13 +43,13 @@ export const ContextMessage = ({ context }) => {
 };
 
 export const Wrapper = ({ context, children }) => {
-  const [{ dispatch: sharedDispatch }, dispatch] = useContext(context);
+  const [{ active }, dispatch] = useContext(context);
   console.log("rendering Wrapper");
   useEffect(() => {
-    dispatch({ type: "enableEffects", payload: dispatch });
+    dispatch({ type: "activate" });
   }, []);
   return (
-    sharedDispatch && (
+    active && (
       <div>
         <h2>Wrapper Component</h2>
         <div>{children}</div>
